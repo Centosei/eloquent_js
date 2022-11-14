c6b1ee1ec6f4729ba7e8d20163c701575cfcb812
centosei <CentoseiSigarette@gmail.com>
2022-11-14 10:13:25 +0000

    the very basics of JS

---
+++ ch1/start.js
@@ -0,0 +1,19 @@
+let num = 100 + 4 * 11
+
+console.log(num)
+console.log(typeof 4.5)
+console.log(typeof "x")
+console.log(NaN == NaN)
+console.log(true ? 1 : 2)
+console.log(false ? 1 : 2)
+
+console.log(8 * null)
+console.log("5" - 1)
+console.log("5" + 1)
+console.log("five" + 2)
+console.log(false == 0)
+console.log(null == undefined)
+console.log(null == 0)
+
+console.log(null || "user")
+console.log("Agnes" || "user")
